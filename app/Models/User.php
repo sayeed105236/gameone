22c2e7238e1ae962661b7a253139fd2e9753cbf2
Abu Sayeed <abusayeed33343536@gmail.com>
2022-04-26 14:41:47 +0600

    user_refferal

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,13 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Questocat\Referral\Traits\UserReferral;
+
 
 class User extends Authenticatable
 {
 
     use HasApiTokens, HasFactory, Notifiable;
-    use UserReferral;
+    
 
     /**
      * The attributes that are mass assignable.
